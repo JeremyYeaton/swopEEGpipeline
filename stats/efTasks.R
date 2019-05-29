78245032ccb0cc9621d5d46ba031c1a2ae27bf10
Jeremy Yeaton <jdyeaton27@gmail.com>
2019-05-29 15:56:46 +0200

    clean behavioral data in python

--- stats/efTasks.R
+++ stats/efTasks.R
@@ -7,6 +7,8 @@ dataStroop = read.table(file = '..\\EFdata\\stroopAll.csv', header = TRUE, sep =
 
 dataNavon = read.table(file = '..\\EFdata\\navonAll.csv', header = TRUE, sep = ',')
 
+dataBeh = read.table(file = '..\\EFdata\\resultsBeh.csv', header = TRUE, sep = ',')
+
 ## Clean the data
 dataStr <- dataStr %>%
   group_by(Type) %>%
